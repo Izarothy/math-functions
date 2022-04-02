687acb737e163c25909567691f95bf49c9ba9f74
Izarothy <izarothmail@gmail.com>
2022-04-02 18:59:47 +0200

    Add point between func

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-export function lineLength(A, B) {
-    return Math.sqrt(Math.pow(B[0] - A[0], 2) + Math.pow(B[1] - A[1], 2));
-}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export function lineLength(A: number[], B: number[]) {
-  return Math.sqrt(Math.pow(B[0] - A[0], 2) + Math.pow(B[1] - A[1], 2));
+export function lineLength(A: number[], B: number[]): number {
+  return Math.sqrt((B[0] - A[0]) ** 2 + (B[1] - A[1]) ** 2);
+}
+
+export function pointBetween(A: number[], B: number[]): number[] {
+  return [(A[0] + B[0]) / 2, (A[1] + B[1]) / 2];
 }
